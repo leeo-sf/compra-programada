ebe95a463866cf447d275a6150eda067bc7b9645
Leonardo Silva <leonardoferreira032@gmail.com>
2026-03-20 11:27:12 -0300

    adiciona validações nas requests adesao e atualizar valor mensal

---
+++ src/CompraProgramada.Application/Validator/AdesaoValidator.cs
@@ -0,0 +1,24 @@
+﻿using CompraProgramada.Application.Request;
+using FluentValidation;
+
+namespace CompraProgramada.Application.Validator;
+
+public sealed class AdesaoValidator : AbstractValidator<AdesaoRequest>
+{
+    public AdesaoValidator()
+    {
+        RuleFor(ad => ad.Nome)
+            .NotEmpty().WithMessage("O nome deve ser preenchido.");
+
+        RuleFor(ad => ad.Cpf)
+            .Matches("^[0-9]*$").WithMessage("O CPF deve conter apenas números.")
+            .MaximumLength(11).WithMessage("O CPF deve conter exatamente 11 caracteres.");
+
+        RuleFor(ad => ad.Email)
+            .NotEmpty().WithMessage("O email deve ser preenchido.")
+            .EmailAddress().WithMessage("O e-mail deve ser válido!");
+
+        RuleFor(ad => ad.ValorMensal)
+            .GreaterThan(0).WithMessage("O valor mensal deve ser superior há zero.");
+    }
+}
---
+++ src/CompraProgramada.Application/Validator/AtualizarValorMensalValidator.cs
@@ -0,0 +1,11 @@
+﻿using CompraProgramada.Application.Request;
+using FluentValidation;
+
+namespace CompraProgramada.Application.Validator;
+
+public sealed class AtualizarValorMensalValidator : AbstractValidator<AtualizarValorMensalRequest>
+{
+    public AtualizarValorMensalValidator()
+        => RuleFor(ad => ad.NovoValorMensal)
+            .GreaterThan(0).WithMessage("O novo valor mensal deve ser superior há zero.");
+}
