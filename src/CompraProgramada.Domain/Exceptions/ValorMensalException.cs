e464a7887ff241fd4b099f7956a672c52ceabf9d
Leonardo Silva <leonardoferreira032@gmail.com>
2026-03-28 22:37:09 -0300

    correção teste com falha

--- src/CompraProgramada.Domain/Exceptions/ValorMensalException.cs
+++ src/CompraProgramada.Domain/Exceptions/ValorMensalException.cs
@@ -1,11 +1,12 @@
 ﻿using CompraProgramada.Domain.Exceptions.Base;
+using System.Globalization;
 
 namespace CompraProgramada.Domain.Exceptions;
 
 public class ValorMensalException : DomainException
 {
     public ValorMensalException(decimal valorMinimoAdesao)
-        : base($"O valor mensal minimo e de R$ {valorMinimoAdesao.ToString("F2")}",
+        : base($"O valor mensal minimo e de R$ {valorMinimoAdesao.ToString("F2", new CultureInfo("pt-BR"))}",
             "VALOR_MENSAL_INVALIDO")
     { }
 }
