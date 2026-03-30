f9f69866a24729acb6cd895a703a1f3731b81a6e
Leonardo Silva <leonardoferreira032@gmail.com>
2026-03-29 22:59:07 -0300

    testes unitários na camada de worker

---
+++ tests/CompraProgramada.Worker.Tests/TestUtils/AppConfigHelper.cs
@@ -0,0 +1,21 @@
+﻿using CompraProgramada.Application.Config;
+using Microsoft.Extensions.Configuration;
+
+namespace CompraProgramada.Worker.Tests.TestUtils;
+
+public class AppConfigHelper
+{
+    public static AppConfig GetAppConfig()
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional: false)
+            .AddJsonFile("appsettings.Development.json", optional: true)
+            .Build();
+
+        AppConfig appConfig = new();
+        configuration.GetSection("ApplicationConfig").Bind(appConfig);
+
+        return appConfig;
+    }
+}
